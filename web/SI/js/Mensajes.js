db64b6fd448ea8121c1cef69320bee952d29b942
DevLucho <64920153+DevLucho@users.noreply.github.com>
2020-06-25 17:54:21 -0500

    Traduccion es - en

--- web/SI/js/Mensajes.js
+++ web/SI/js/Mensajes.js
@@ -1,18 +1,40 @@
 function Mensaje(titulo, texto, icono) {
     Swal.fire({title: titulo, text: texto, icon: icono, showConfirmButton: false, timer: 2000});
-}
+};
 
+function Mensajes(titulo, texto, icono) {
+    Swal.fire({title: titulo, text: texto, icon: icono});
+};
+
+function Confirmar(titulo, texto, icono, textob, tituloc, textoc, iconoc) {
+    Swal.fire({
+        title: titulo,
+        text: texto,
+        icon: icono,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: textob,
+        showCancelButton: true,
+        cancelButtonText: 'No',
+        cancelButtonColor: '#d33'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(tituloc, textoc, iconoc)
+        }
+    })
+};
+
+// Mensaje para redirigir (index)
 function ConfirmacionResidente(linkURL) {
     Swal.fire({
         icon: 'success',
         title: "Registro exitoso",
-        confirmButtonText: 'Iniciar sesi&oacute;n',
+        confirmButtonText: 'Iniciar sesi&oacute;n'
     }).then(function (result) {
         console.log(result);
         if (result.value) {
             window.location.href = linkURL;
         }
     });
-}
+};
 
 
