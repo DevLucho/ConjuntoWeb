bb875ccee82477db4f925fd6371b7e6ac55d7123
Avendaño <jsavendano62@misena.edu.co>
2020-08-09 02:16:07 -0500

    mañana termino

---
+++ src/java/controlador/validacionVisitante.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author Sebastian
+ */
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+@FacesValidator("valnu")
+public class validacionVisitante implements Validator{
+    @Override
+    public void validate(FacesContext context, UIComponent comp, Object value){
+        String texto = (String)value;
+        
+        if(texto.length() < 4 || texto.length() > 10){
+            FacesMessage msg = new FacesMessage("Apellido no valido por tamaño");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+    }
+}
