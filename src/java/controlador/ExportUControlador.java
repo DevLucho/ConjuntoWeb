f92eaf2323e0c10496d45de088348e4e2e3c1f66
User <User@DESKTOP-2B8C9Q9>
2020-09-09 00:54:31 -0500

    export

--- src/java/controlador/ExportUControlador.java
+++ src/java/controlador/ExportUControlador.java
@@ -15,6 +15,7 @@ import java.util.Iterator;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.servlet.http.Part;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
@@ -31,6 +32,8 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 @ManagedBean
 public class ExportUControlador {
      Part excel;
+    @Inject
+    private MensajeControlador mensaje;
 
     public Part getExcel() {
         return excel;
@@ -100,12 +103,16 @@ public class ExportUControlador {
                 }          
             }
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Aviso","Migracion Exitosa"));
+            mensaje.setMensaje("Mensaje('Importacion Exitosa','Datos registrados en la Bd','success');");
         }catch (IOException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Aviso","Error abriendo archivo"));        
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Aviso","Error abriendo archivo"));  
+            mensaje.setMensaje("Mensaje('Error en Importacion','El archivo no se puede abrir, revisar la extension requerida','warning');");
         }catch (InvalidFormatException e){
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Aviso","Error en formato"));                
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Aviso","Error en formato1"));  
+            mensaje.setMensaje("Mensaje('Error en Importacion','El formato en el archivo es erroneo, por favor verificar las instrucciones.','error');");
         }catch (SQLException e){
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Aviso","Error en formato"));                
+            mensaje.setMensaje("Mensaje('Error en Importacion','El formato en el archivo es erroneo, por favor verificar las instrucciones.','error');");
         }
     }
 }
